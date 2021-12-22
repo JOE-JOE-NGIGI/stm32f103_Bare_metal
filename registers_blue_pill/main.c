63220b7dc2127522957367cb8c98f11613086f39
JOE-JOE-NGIGI <jngigiw@gmail.com>
2021-12-22 23:05:49 +0300

    blink led

---
+++ registers_blue_pill/main.c
@@ -0,0 +1,24 @@
+#include "stm32f10x.h"                  // Device header
+//macro for writing 0 and 1 to the bit
+
+#define BitSet(arg,posn) ((arg) | (1L << (posn)))
+#define BitClr(arg,posn), ((arg) & ~(1L << (posn)))
+
+int main () 
+	{
+		RCC->APB2ENR |= (1<<4); //enable bit 4 of the APB2ENR register(Enable Clock)
+	
+		//Configure Port C pin 13 as Push-Pull output
+		GPIOC->CRH &= ~((1<<23) | (1<<22) | (1<<20));
+		GPIOC->CRH |= (1<<21);
+		
+		while(1) {
+			GPIOC->ODR ^= (1<<13);
+			
+			
+		}
+	
+	
+	}
+	
+	
